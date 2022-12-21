a4a9a8900388f76c874f2215718ff9111c7b8b30
DonovanZeanah <donovan.zeanah@outlook.com>
2022-12-21 23:36:43 +0900

    constants changed.

--- ContactWebEFCore6/Models/ContactCacheConstants.cs
+++ ContactWebEFCore6/Models/ContactCacheConstants.cs
@@ -2,6 +2,7 @@ namespace ContactWebEFCore6.Models
 {
   public class ContactCacheConstants
   {
-    public const string statesData = "statesData";
+    public const string ALL_STATES_DATA_SESSION = "all_states_cache_data";
+    public const string ALL_STATES_DATA = "all_states_cache_data";
   }
 }
