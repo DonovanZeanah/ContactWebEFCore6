1c3b3992c0104ae3e7c2c6dbbb961c42b9d60370
DonovanZeanah <donovan.zeanah@outlook.com>
2022-12-22 00:36:43 +0900

    ContactsRepository built.

---
+++ MyContactManagerRepositories/ContactsRepository.cs
@@ -0,0 +1,118 @@
+﻿using ContactWebModels;
+using Microsoft.EntityFrameworkCore;
+using MyContactManagerData;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContactManagerRepositories
+{
+  public class ContactsRepository : IContactsRepository
+  {
+    private readonly MyContactManagerDbContext _context;
+
+    public ContactsRepository(MyContactManagerDbContext context)
+    {
+      _context = context;
+    }
+
+    public async Task<List<Contact>> GetAllAsync()
+    {
+      var allContactsData = await _context.Contacts.AsNoTracking().OrderBy(x => x.LastName).ToListAsync();
+      return allContactsData;
+    }
+
+    public async Task<Contact> GetAsync(int id)
+    {
+      var contact = await _context.Contacts.AsNoTracking().SingleOrDefaultAsync(x => x.Id == id);
+      return contact;
+    }
+    public async Task<int> AddOrUpdateAsync(Contact contact)
+    {
+      if (contact.Id == 0)
+      {
+        return await Insert(contact);
+      }
+      return await Update(contact);
+    }
+
+    private async Task<int> Insert(Contact contact)
+    {
+      await _context.Contacts.AddAsync(contact);
+      await _context.SaveChangesAsync();
+      return contact.Id;
+    }
+
+
+
+
+    private async Task<int> Update(Contact contact)
+    {
+      var existingContact = await _context.Contacts.SingleOrDefaultAsync(x => x.Id == contact.Id);
+
+      if (existingContact == null)
+      {
+        throw new Exception("contact not found homeboia");
+      }
+      existingContact.Birthday = contact.Birthday;
+      existingContact.City = contact.City;
+      existingContact.Email = contact.Email;
+      existingContact.FirstName = contact.FirstName;
+      existingContact.LastName = contact.LastName;
+      existingContact.PhonePrimary = contact.PhonePrimary;
+      existingContact.PhoneSecondary = contact.PhoneSecondary;
+      existingContact.Zip = contact.Zip;
+      existingContact.StreetAddress1 = contact.StreetAddress1;
+      existingContact.StreetAddress2 = contact.StreetAddress2;
+
+
+
+
+      return existingContact.Id;
+
+    }
+
+    public async Task<bool> ExistsAsync(int id)
+    {
+      return await _context.Contacts.AsNoTracking().AnyAsync(x => x.Id == id);
+
+    }
+
+
+    public async Task<int> DeleteAsync(int id)
+    {
+      var contactToDelete = await GetAsync(id);
+      try
+      {
+        _context.Remove(contactToDelete);
+        _context.SaveChanges();
+        return id;
+      }
+      catch
+      {
+        return 0;
+      }
+    }
+
+
+
+    public async Task<int> DeleteAsync(Contact contact)
+    {
+      return await DeleteAsync(contact.Id);
+    }
+
+    Task<List<State>> IContactsRepository.GetAllAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    Task<State> IContactsRepository.GetAsync(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ MyContactManagerRepositories/IContactsRepository.cs
@@ -0,0 +1,19 @@
+﻿using ContactWebModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContactManagerRepositories
+{
+  public interface IContactsRepository
+  {
+    Task<List<State>> GetAllAsync();
+    Task<State> GetAsync(int id);
+    Task<int> AddOrUpdateAsync(Contact contact);
+    Task<int> DeleteAsync(int id);
+    Task<int> DeleteAsync(Contact contact);
+    Task<bool> ExistsAsync(int id);
+  }
+}
